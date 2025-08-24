f5c3f814a4468d99de80b128fbb471548e2b89bf
Salihxmod <semetsalih344@gmail.com>
2025-08-24 16:57:34 +0300

    Encapsulation Work Added

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ögrenme_Amaçlı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kapsülleme:Property(Özellik) ler aracılıgyla class içerisinde
+            //orjinal field ı dışarıdan erişilmesini engelleyerek korumaya alarak
+            //Field içerisine ilğili veriyi kontrollü bir şekilde property ler aracılıgıyla içerisine atamak.
+
+
+            //Property(Özellik): Methotlar ve Fieldların birleşimi/karışımı. Yani
+            //Propertyler Hem Methodlar Gibi Hemde Fieldlar gibi kullanılabiliyor.
+
+            Vatandaş vatandaş = new Vatandaş();
+            vatandaş.TCNO = ""; 
+        }
+    }
+}
---
+++ Encapsulation/Vatandaş.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ögrenme_Amaçlı
+{
+    internal class Vatandaş
+    {
+        string tcNo = "";
+        internal string TCNO 
+        {
+            get
+            {
+                return tcNo;
+            }
+            set
+            {
+                if(value != "" && value.Length == 11 && value.StartsWith("0"))
+                {
+                    tcNo = value;
+                }
+            }
+        }
+
+
+    }
+}
