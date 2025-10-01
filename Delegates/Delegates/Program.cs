f2efc8a69c1fd351d7c641e17092b258bc6a78db
Salihxmod <semetsalih344@gmail.com>
2025-10-01 11:30:01 +0300

    Delegates

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+
+namespace Delegates
+{
+    delegate int MathDelegate(int number1, int number2);
+    delegate void LoggerDelegate(string message);
+    delegate bool numbercontrolDelegate(int number);
+    class Program()
+    {
+        
+        static void Main()
+        {
+            Methots methots = new Methots();
+            LoggerDelegate logger = methots.ConsoleLogger;
+            MathDelegate mathDelegate = methots.Add;
+            Console.WriteLine($"{mathDelegate.Invoke(5,3)}");
+            mathDelegate += methots.Subtract;
+            Console.WriteLine($"{mathDelegate.Invoke(5,3)}");
+            mathDelegate += methots.Multiply;
+            Console.WriteLine($"{mathDelegate.Invoke(5,3)}");
+            mathDelegate += methots.Divide;
+            Console.WriteLine($"{mathDelegate.Invoke(5,3)}");
+            logger("Selamun Aleyküm Dünya");
+            logger = methots.FileLogger;
+            logger("Selamun Aleyküm Dünya");
+            methots.Check(5,methots.isEvenNumber);
+            methots.Check(18,methots.isPositive);
+        }
+    }   
+    class Methots
+    {
+        internal  int  Add(int number1, int number2) => number1 + number2;
+        internal  int  Subtract(int number1, int number2) => number1 - number2;
+        internal  int  Multiply(int number1, int number2) => number1 * number2;
+        internal  int  Divide(int number1, int number2) => number1 / number2;
+        internal  void ConsoleLogger(string message) => Console.WriteLine(message);
+        internal  void FileLogger(string message) => File.AppendAllText("log.txt",message);
+        internal  bool isEvenNumber(int number) => number % 2 == 0;
+        internal  bool isPositive(int number) => number > 0;
+
+        internal  void Check(int number, numbercontrolDelegate numbercontrol)
+        {
+            if (numbercontrol(number))
+            {
+                Console.WriteLine($"{number} Sayısı Koşulu Saglıyor");
+            }
+            else
+            {
+                Console.WriteLine($"{number} Sayısı Koşulu Saglamıyor");
+            }
+        }
+    }
+}
+
