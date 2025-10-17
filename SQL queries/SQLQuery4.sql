cdf061e13c4d435f2dc6600e1138d100d1addcfc
Salihxmod <semetsalih344@gmail.com>
2025-10-17 17:44:56 +0300

    New SQL queries  Added

---
+++ SQL queries/SQLQuery4.sql
@@ -0,0 +1,44 @@
+﻿--DDl(Data Definition Language)
+
+--Create -- Oluşturma
+-- Alter -- Güncelleme
+-- Drop -- kaldırma
+
+use master 
+create database OkulDb
+
+use OkulDb
+create table Ogrenciler(
+Id int Primary key Identity(1,1),
+Ad nvarchar(50) not null,
+Soyad nvarchar(50) not null,
+DogumTarihi Date null);
+
+
+-- Tablo Adı Güncelleme
+
+exec sp_rename 'Ogrenciler','OgrenciListesi'
+-- Tabloyu Güncelleyerek Yeni Bir Kolon Ekleme
+alter table OgrenciListesi 
+add Email nvarchar(100) null
+
+--Var Olan Bir Tablo İçerisindeki var Olan Bir kolonu Güncelleme
+
+alter table OgrenciListesi
+alter column Email nvarchar(200) null
+
+--Var Olan Bir Tablo İçerisinden Kolonu Kaldırma
+alter table OgrenciListesi
+drop column Email
+
+--Var Olan Bir Tabloyu Kaldırma 
+drop table OgrenciListesi 
+
+use master 
+drop database OkulDB
+
+-- İlişki Türleri
+
+-- Bire Bir --
+-- Bire Çok --
+-- Çoka Çok --
---
+++ SQL queries/SQLQuery5.sql
@@ -0,0 +1,18 @@
+﻿use Northwind
+
+--select Country from Customers group by Country
+
+select Country,City from Customers group by Country,City
+
+-- Agreate Functions : Bütün Sql uygulamalarında kullanabilicemiz ortak Fonksiyonlardır. Yardımcı Araçlardır.
+-- Count : Sayısını Verir
+-- Sum   : Kolonun Toplamını Verir
+-- Max   : Kolon İçerisindeki En Büyük degeri Verir.
+-- Min   : Kolon İçerisindeki En Küçük degeri Verir.
+-- Avg   : Kolon İçerisindeki Degerlerin Ortalamasını Verir.
+-------------------------
+
+select Country,City,Count(City) from Customers group by Country,City
+
+
+
