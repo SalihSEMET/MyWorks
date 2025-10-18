05d2671345615091df4727bcdcc87bc5eac87a15
Salihxmod <semetsalih344@gmail.com>
2025-10-18 19:15:46 +0300

    Added SQL Query8

--- SQL queries/SQLQuery8.sql
+++ SQL queries/SQLQuery8.sql
@@ -1,2 +1,28 @@
 use Northwind
 
+Select TOP(5) * from Products order by UnitPrice desc
+
+SELECT
+  Orders.ShipCountry,
+  "Order Details".Quantity * "Order Details".UnitPrice
+FROM Orders
+JOIN "Order Details" ON Orders.OrderID = "Order Details".OrderID
+
+SELECT
+  Orders.ShipCountry,
+  SUM("Order Details".Quantity * "Order Details".UnitPrice) AS ToplamSatis
+FROM Orders
+JOIN "Order Details" ON Orders.OrderID = "Order Details".OrderID
+GROUP BY Orders.ShipCountry order by ToplamSatis desc
+
+SELECT
+  ShipCountry,
+  COUNT(*) AS ToplamSiparisSayisi
+FROM Orders
+GROUP BY ShipCountry
+
+SELECT TOP(1)
+EmployeeID,
+COUNT(*) AS ToplamSatisAdedi
+from Orders
+group by EmployeeID order by ToplamSatisAdedi desc 
