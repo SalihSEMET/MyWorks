5fc584beb6d52c74099f11693aa5795fc505d1b5
Salihxmod <semetsalih344@gmail.com>
2025-10-18 08:58:02 +0300

    Update Sql Query6

--- SQL queries/SQLQuery6.sql
+++ SQL queries/SQLQuery6.sql
@@ -39,3 +39,55 @@ select FirstName,LastName,City from Employees
 
 select Country,COUNT(*) MusteriSayısı
 from Customers group by Country order by MusteriSayısı desc
+
+-- Sadece 1997 Yılındaki Siparişlerin Listelenmesi
+
+select * from Orders where Year(OrderDate) = 1997
+
+select * from Orders where Month(OrderDate) = 7
+-- Len KArakter Uzulugunu Verir
+select ProductName,LEN(ProductName)  karaktersayısı from Products
+
+
+select FirstName + ' ' + LastName from Employees
+
+-- Replicate : Belirtilen Sayı Adedince Karakter Ekler 
+
+-- Len : Metnin Uzunlugunu/Karakter Sayısını Verir
+
+-- Concat : Birden Fazla Veriyi Yan Yana Birleştirme İşlemi Yapar 
+
+-- Substring : Bir Verinin Belirli Bir Başlangıç Degerinden İtibaren Belirli Bir Sayı Kadar Karakterlerini Verir. 
+
+
+select CONCAT(SUBSTRING(FirstName,1,1),REPLICATE('*',Len(FirstName) - 1),
+' ',Substring(LastName,1,1),REPLICATE('*',LEN(LastName) - 1))
+from Employees
+
+
+-- Subquery(Alt Sorgu) : Bir Sorgunun Sonucunun Başka Bir Sorgunun Sonucundan Dönen Degere göre Çalışması.
+-- Subquery İki Yere Yazılır select ve from arasına birde where koşulundan sonra.
+
+select * from Categories
+select * from Products where CategoryID = (select CategoryID from Categories where CategoryName = 'Seafood')
+
+select
+ProductID,
+ProductName,
+SupplierID,
+(select CategoryName from Categories where CategoryID = Products.CategoryID),
+QuantityPerUnit,
+UnitPrice,
+UnitsOnOrder,
+ReorderLevel,
+Discontinued
+from Products
+
+select
+OrderID,
+(select ProductName from Products where ProductID = [Order Details].ProductID),
+UnitPrice,
+Quantity,
+Discount
+from [Order Details]
+
