09a716d8a651ac0c3170796c2b6a5d22ddfa2bd5
Salihxmod <semetsalih344@gmail.com>
2025-10-18 13:03:27 +0300

    Update 18-10-2025

---
+++ Websites/18-10-2025/.idea/index.js
@@ -0,0 +1,10 @@
+// Değişken Tanımlama
+
+
+var Name = "Ali yıldız";
+let age = 26;
+const job = "Mühendis"
+
+// var: Eski Bir Yöntemdir Globalde Yada Fonksiyon İçerisinde Tanımlanır. Aynı Değişken İsmiyle Oluşturuldugunda Hata Vermez
+// let: Modern Yöntemdir Sadece Tanımlandıgı Blokta Geçerlidir ve Aynı Değişken İsmiyle Daha Once Olusturuldu İse Hara Verir.
+// const : Sabittir, Değeri Sonradan Değiştirilemez.
