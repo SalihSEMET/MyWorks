e0f3c69bbbdfe11e2fdfaa6c8e4f75dcb9e8bf7a
Salihxmod <semetsalih344@gmail.com>
2025-10-18 16:33:48 +0300

    Added SQL Query7

---
+++ SQL queries/SQLQuery7.sql
@@ -0,0 +1,29 @@
+﻿create Database MyNorthwind
+use MyNorthwind
+create Table Kampanya(KampanyaID INT PRIMARY KEY IDENTITY(1,1)
+,KampanyaAdı NVARCHAR(100),
+IndirimOrani DECIMAl(5,2),
+BaslangicTarihi Date,
+BitisTarihi Date);
+
+INSERT INTO Kampanya (KampanyaAdı, IndirimOrani, BaslangicTarihi, BitisTarihi)
+VALUES
+('3 Alana 1 Bedava', 0.33, '2025-10-17', '2025-10-31');
+
+INSERT INTO Kampanya (KampanyaAdı, IndirimOrani, BaslangicTarihi, BitisTarihi)
+VALUES
+('Kış Festivali', 0.20, '2025-12-01', '2025-12-31');
+
+INSERT INTO Kampanya (KampanyaAdı, IndirimOrani, BaslangicTarihi, BitisTarihi)
+VALUES
+('Eylül Fırsatı', 0.15, '2025-09-01', '2025-09-30');
+
+UPDATE Kampanya
+SET IndirimOrani = 0.25
+WHERE KampanyaAdı = 'Kış Festivali';
+
+
+DELETE FROM Kampanya
+WHERE KampanyaAdı = 'Eylül Fırsatı';
+
+
