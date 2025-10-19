50b972bfcfb829a47bee57acca138494b114eba2
Salihxmod <semetsalih344@gmail.com>
2025-10-19 18:05:05 +0300

    Added SQL Queries 10

--- SQL queries/SQLQuery10.sql
+++ SQL queries/SQLQuery10.sql
@@ -8,3 +8,15 @@ WHERE
     UnitPrice = (SELECT MAX(UnitPrice) FROM Products);
 
 
+CREATE VIEW vw_TopCustomers AS
+SELECT TOP 10
+    Customers.CompanyName AS MusteriAdi,
+    COUNT(Orders.OrderID) AS ToplamSiparisSayisi
+FROM
+    Customers
+INNER JOIN
+    Orders ON Customers.CustomerID = Customers.CustomerID
+GROUP BY
+    Customers.CustomerID, Customers.CompanyName
+ORDER BY
+    ToplamSiparisSayisi DESC;
