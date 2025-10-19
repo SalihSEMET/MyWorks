4761f999986ed57a3f71b69f93ad49ea89cae81b
Salihxmod <semetsalih344@gmail.com>
2025-10-19 18:00:09 +0300

    Added Sql Query 9

--- SQL queries/SQLQuery9.sql
+++ SQL queries/SQLQuery9.sql
@@ -1,1 +1,23 @@
-use Northwind
+﻿use Northwind
+
+SELECT
+    Customers.CompanyName AS MusteriAdi,
+    Orders.OrderDate AS SiparisTarihi
+FROM
+    Customers
+INNER JOIN
+    Orders ON Customers.CustomerID = Orders.CustomerID
+ORDER BY
+    MusteriAdi, SiparisTarihi;
+
+
+SELECT
+    ProductID,
+    UnitPrice,
+    CASE
+        WHEN UnitPrice <= 10 THEN 'Ucuz'
+        WHEN UnitPrice > 10 AND UnitPrice <= 30 THEN 'Orta'
+        ELSE 'Pahalı'
+    END AS FiyatKategorisi
+FROM
+    [Order Details];
