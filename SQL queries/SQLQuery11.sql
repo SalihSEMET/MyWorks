9146e4f4ed718ec2156b8c006b7251705166b029
Salihxmod <semetsalih344@gmail.com>
2025-10-19 18:22:53 +0300

    Added SQL Query 11

---
+++ SQL queries/SQLQuery11.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE sp_MusteriSiparisleri
+    @MusteriID nchar(5)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    SELECT
+        C.CompanyName,
+        O.OrderID,
+        O.OrderDate,
+        O.ShipCity,
+        O.ShipCountry
+    FROM
+        Orders AS O
+    INNER JOIN
+        Customers AS C ON O.CustomerID = C.CustomerID
+    WHERE
+        O.CustomerID = @MusteriID
+    ORDER BY
+        O.OrderDate DESC;
+END;
+GO  
+
+CREATE PROCEDURE sp_UrunFiyatGuncelle
+    @UrunID int,
+    @YeniFiyat money
+AS
+BEGIN
+    SET NOCOUNT ON;
+    UPDATE
+        Products
+    SET
+        UnitPrice = @YeniFiyat
+    WHERE
+        ProductID = @UrunID;
+END;
